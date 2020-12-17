6d361983d1dbd44f7a5c05f4b845d6b0ebcd91e9
Jurn Ho <jurn@magicmonster.com>
2020-12-17 13:46:44 +0000

    quote reservation: fix response type

--- raml-to-jaxrs/raml-to-jaxrs-cli/src/main/java/org/raml/jaxrs/ramltojaxrs/fix.sh
+++ raml-to-jaxrs/raml-to-jaxrs-cli/src/main/java/org/raml/jaxrs/ramltojaxrs/fix.sh
@@ -52,3 +52,4 @@ grep --files-with-matches "Root" java/com/triplake/adapter/kmapi/jaxrs/api/model
 
 # fix up resource.. remove delegate
 sed -i 's/PostFlightsSearchResponse postFlightsSearch/SearchFlightResponse postFlightsSearch/g' java/com/triplake/adapter/kmapi/jaxrs/api/resource/Flights.java
+sed -i 's/PostReservationsQuoteResponse postReservationsQuote/QuoteReservationResponse postReservationsQuote/g' java/com/triplake/adapter/kmapi/jaxrs/api/resource/Reservations.java
